2c73fda7ab4f27db94cee72ed62ff4c7fdd8eab7
Alexey Zhokhov <alexey@zhokhov.com>
2016-01-24 03:16:33 +0800

    CircleCI.

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+export TERM="dumb"
+
+printf "\n> \e[1;37mBuilding react-asset-pipeline\e[0m\n"
+
+set -e
+
+ABSOLUTE_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+cd $ABSOLUTE_PATH
+
+printf "\n# Checking for failed dependencies\n\n"
+./gradlew dependencies | grep FAILED && exit 1
+
+if [ -n "${ARTIFACTORY_CONTEXT_URL}" ]; then
+    printf "\n# Building jar\n\n"
+    ./gradlew jar
+
+    printf "\n# Building jar with sources\n\n"
+    ./gradlew sourcesJar
+
+    printf "\n# Publishing plugin to Artifactory\n\n"
+    ./gradlew artifactoryPublish
+else
+    printf "# Installing library to Maven local\n\n"
+    ./gradlew install
+fi
